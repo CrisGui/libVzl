9839ddf16d4774e4084a93d871d53c638884d4e6
Cristian Guilarte <159936410+CrisGui@users.noreply.github.com>
2024-08-01 11:28:15 +0100

    test: Use case of result type

--- sandbox/main.c
+++ sandbox/main.c
@@ -1,5 +1,18 @@
+#include <libVzl/Types.h>
 #include <stdio.h>
+
 int main(/*int argc, char *argv[]*/){
   printf("Hello from sandbox\n");
+
+  int const val = 12;
+
+  vzl_typeResultInt const r = vzl_wrapValueInt(true, val, vzl_errorNull);
+
+  if(vzl_unwrapSuccessInt(&r)){
+    printf("Result: %d\n", vzl_unwrapValueInt(&r));
+  } else {
+    printf("Error: %d\n", vzl_unwrapErrorInt(&r));
+  }
+
   return 0;
 }
